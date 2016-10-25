583cbc0b1d681775f997924797340629d0d99a6c
John Vivian <jtvivian@gmail.com>
2016-10-25 01:05:50 -0700

    Import mkdir_p

--- pairwise_gtex_tcga/pairwise_de.py
+++ pairwise_gtex_tcga/pairwise_de.py
@@ -17,6 +17,7 @@ import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 
+from utility_functions import mkdir_p
 from utility_functions.gtf_parser import GTFRecord
 
 logging.basicConfig(level=logging.INFO)
