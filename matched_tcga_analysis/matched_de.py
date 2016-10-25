1a4f6ef40fb8a6b85f5f9ec35ecc8a0c38798d4b
John Vivian <jtvivian@gmail.com>
2016-10-25 01:06:23 -0700

    Refactor matched analysis for new project strucure

--- matched_tcga_analysis/matched_de.py
+++ matched_tcga_analysis/matched_de.py
@@ -1,4 +1,4 @@
-import errno
+import argparse
 import logging
 import os
 import subprocess
@@ -7,47 +7,24 @@ import textwrap
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 
-from pairwise_gtex_tcga.pairwise_de import PairwiseAnalysis
+from utility_functions import mkdir_p
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-remove_protein_coding_genes = PairwiseAnalysis.remove_nonprotein_coding_genes
-
-
-# REFACTOR OUT AT SOME POINT
-gencode_path = '/mnt/metadata/gencode.v23.annotation.gtf'
-
-
-def create_matched_dataframe(df_path, dest):
-    """
-    Accepts <tissue>_combined.tsv (or combined_pc.tsv) and returns <tissue>_tcga_matched_pairs.tsv
-
-    Checks if the tsv has protein-coding genes in it.
-
-    :param str df_path:
-    :param str dest:
-    :return:
-    """
-    df = pd.read_csv(df_path, sep='\t', index_col=0)
-    if '_pc.tsv' not in df_path:
-        log.info('Removing protein coding genes')
-        output_path = os.path.splitext(df_path)[0] + '_pc.tsv'
-        df = remove_protein_coding_genes(df, gencode_path, output_path=output_path)
+def create_matched_dataframe(input_df, output_df):
+    df = pd.read_csv(input_df, sep='\t', index_col=0)
     barcodes = [x[:-3] for x in df.columns]
     matched = list(set(x for x in barcodes if x + '-11' in df.columns and x + '-01' in df.columns))
     # Write out the patients here
-    log.info('Writing out patient information to: ' + dest)
-    patient_path = os.path.join(dest, 'patients')
+    patient_path = os.path.join(os.path.dirname(output_df), 'patient-pairs.txt')
     with open(patient_path, 'w') as f:
         f.write('\n'.join([x for x in matched for _ in (0, 1)]))
     matched = [f(x) for x in matched for f in (lambda f1: f1 + '-01', lambda f2: f2 + '-11')]
     matched_df = df[matched]
-    log.info('Writing out matched dataframe to: ' + dest)
-    match_df_path = os.path.join(dest, 'matched_df.tsv')
-    matched_df.to_csv(match_df_path, sep='\t')
-    return match_df_path, patient_path
+    log.info('Writing out matched dataframe to: ' + os.path.dirname(output_df))
+    matched_df.to_csv(output_df, sep='\t')
 
 
 def generate_match_de():
@@ -61,7 +38,7 @@ def generate_match_de():
     script.dir <- dirname(sys.frame(1)$ofile)
 
     # Read in tables / patients
-    n <- read.table(df_path, sep='\t', header=1, row.names=1)
+    n <- read.table(df_path, sep='\\t', header=1, row.names=1)
     patients <- read.table(patient_path)
 
     # Create matrix vectors
@@ -80,23 +57,23 @@ def generate_match_de():
 
     # Write out table
     resOrdered <- res[order(res$padj),]
-    res_name <- paste(script.dir, 'results.csv', sep='/')
-    write.csv(as.data.frame(resOrdered), file=res_name)
+    res_name <- paste(script.dir, 'results.tsv', sep='/')
+    write.table(as.data.frame(resOrdered), file=res_name, col.names=NA, sep='\\t')
 
     # MA Plot
-    ma_name <- paste(script.dir, 'MA.pdf', sep='/')
+    ma_name <- paste(script.dir, 'plots', 'MA.pdf', sep='/')
     pdf(ma_name, width=7, height=7)
     plotMA(res, main='DESeq2')
     dev.off()
 
     # Dispersion Plot
-    disp_name <- paste(script.dir, 'disp.pdf', sep='/')
+    disp_name <- paste(script.dir, 'plots', 'dispersion.pdf', sep='/')
     pdf(disp_name, width=7, height=7)
     plotDispEsts( y, ylim = c(1e-6, 1e1) )
     dev.off()
 
     # PVal Hist
-    hist_name <- paste(script.dir, 'pval-hist.pdf', sep='/')
+    hist_name <- paste(script.dir, 'plots', 'pval-hist.pdf', sep='/')
     pdf(hist_name, width=7, height=7)
     hist( res$pvalue, breaks=20, col="grey" )
     dev.off()
@@ -106,7 +83,7 @@ def generate_match_de():
     bins <- cut( res$baseMean, qs )
     levels(bins) <- paste0("~",round(.5*qs[-1] + .5*qs[-length(qs)]))
     ratios <- tapply( res$pvalue, bins, function(p) mean( p < .01, na.rm=TRUE ) )
-    ratio_name <- paste(script.dir, 'ratios.pdf', sep='/')
+    ratio_name <- paste(script.dir, 'plots', 'ratios.pdf', sep='/')
     pdf(ratio_name, width=7, height=7)
     barplot(ratios, xlab="mean normalized count", ylab="ratio of small $p$ values")
     dev.off()
@@ -114,60 +91,52 @@ def generate_match_de():
 
 
 def write_de_script(directory):
-    deseq_script_path = os.path.join(directory, 'matched_deseq2.R')
+    deseq_script_path = os.path.join(directory, 'deseq2.R')
     with open(deseq_script_path, 'w') as f:
         f.write(generate_match_de())
     return deseq_script_path
 
 
-def run_deseq2(deseq_path, matched_df_path, patient_path):
-    """
-
-    :param deseq_path:
-    :param matched_df_path:
-    :param patient_path:
-    :return:
-    """
-    log.info('Running sample: ' + matched_df_path)
-    p = subprocess.Popen(['Rscript', deseq_path, matched_df_path, patient_path])
+def run_deseq2(tissue_dir):
+    log.info('Running sample: ' + tissue_dir)
+    patient_path = os.path.join(tissue_dir, 'patient-pairs.txt')
+    df_path = os.path.join(tissue_dir, 'matched-tcga-counts.tsv')
+    deseq_path = os.path.join(tissue_dir, 'deseq2.R')
+    p = subprocess.Popen(['Rscript', deseq_path, df_path, patient_path])
     out, err = p.communicate()
     if not p.returncode == 0:
         raise RuntimeError('DESeq run failed!\n\n\nstdout:\n{}stderr:\n{}\n\n\n'.format(out, err))
     return 'yay!'
 
 
-def run_deseq2_wrapper(blob):
-    deseq_path, matched_df_path, patient_path = blob
-    run_deseq2(deseq_path, matched_df_path, patient_path)
-
-
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
-
-
 def main():
-    tissues = [os.path.join('/mnt/tissues', x) for x in os.listdir('/mnt/tissues')]
-    deseq2_info = []
-    for tissue in tissues:
-        df_path = [os.path.join(tissue, x) for x in os.listdir(tissue) if '_combined' in x]
-        if df_path:
-            print tissue
-            match_dir = os.path.join(tissue, 'matched_TCGA_DE')
-            mkdir_p(match_dir)
-            deseq_path = write_de_script(match_dir)
-            df_path = df_path[0] if len(df_path) == 1 else [x for x in df_path if 'pc.tsv' in x][0]
-            print df_path
-            match_df_path, patient_path = create_matched_dataframe(df_path, dest=match_dir)
-            deseq2_info.append((deseq_path, match_df_path, patient_path))
+    """
+    First run create_project.py
 
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        executor.map(run_deseq2_wrapper, deseq2_info)
+    Runs DESeq2 for all TCGA samples with a tumor-normal pair
+    :return:
+    """
+    parser = argparse.ArgumentParser(description=main.__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--rna-seq-directory', type=str, help='Full path to rna-seq_analysis directory, created by'
+                                                              'running create_project.py')
+    parser.add_argument('--cores', type=int, help='Number of cores to use when running R.', default=1)
+    params = parser.parse_args()
+    # Create subdirectories
+    root_dir = params.rna_seq_directory
+    experiment_dir = os.path.join(root_dir, 'experiments/matched-TCGA-tumor-normal')
+    tissue_pairs = os.listdir(os.path.join(root_dir, 'data/tissue-pairs'))
+    [mkdir_p(os.path.join(experiment_dir, x, 'plots')) for x in tissue_pairs]
+    # Create matched dataframes
+    for tissue in tissue_pairs:
+        input_df = os.path.join(root_dir, 'tissue-pairs', tissue, 'combined-gtex-tcga-counts-protein-coding.tsv')
+        output_df = os.path.join(experiment_dir, tissue, 'matched-tcga-counts.tsv')
+        create_matched_dataframe(input_df, output_df)
+        # Write out DESeq2 R script
+        write_de_script(os.path.join(experiment_dir, tissue))
+    # Run DESeq2 in batches
+    log.info('Starting DESeq2 run using {} number of cores'.format(params.cores))
+    with ThreadPoolExecutor(max_workers=params.cores) as executor:
+        executor.map(run_deseq2, [os.path.join(experiment_dir, tissue) for tissue in tissue_pairs])
 
 
 if __name__ == '__main__':
