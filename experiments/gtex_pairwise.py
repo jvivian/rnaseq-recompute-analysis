f8ef1735b98e78bfbafc5460496576f6bd001eff
John Vivian <jtvivian@gmail.com>
2017-02-20 13:03:08 -0800

    Fix GTEx pairwise experiment
    Now computationally tractable by flipping the “hold” group
    Fixed bugs related to combination of results
    Fixed R subsetting nonsense

--- experiments/gtex_pairwise.py
+++ experiments/gtex_pairwise.py
@@ -1,15 +1,16 @@
+import logging
 import os
+import pickle
 import textwrap
 from collections import defaultdict
 
+import numpy as np
+import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
 from experiments.AbstractExperiment import AbstractExperiment
-import logging
-from tqdm import tqdm
-import pandas as pd
-import numpy as np
-from utils import run_deseq2, add_gene_names
+from utils import run_deseq2
 from utils import write_script
 
 logging.basicConfig(level=logging.INFO)
@@ -42,19 +43,19 @@ class GtexPairwise(AbstractExperiment):
         else:
             all_samples_vector = set(open(self.output_df, 'r').readline().strip().split('\t'))
 
-        # A vector consists of all GTEx samples NOT part of the current tissue,
-        # plus one sample from that tissue at the end of the list
+        # A vector consists of all current tissue samples + one for each sample NOT in this set
         log.info('Writing out vectors')
         for df in tqdm(self.protein_coding_paths):
             tissue = os.path.basename(os.path.dirname(df))
             tissue_set = {x for x in open(df, 'r').readline().strip().split('\t') if 'GTEX' in x}
-            for sample in tissue_set:
-                sample_vector = list(all_samples_vector - tissue_set) + [sample]
+            for sample in (all_samples_vector - tissue_set):
+                sample_vector = [x.replace('-', '.') for x in list(tissue_set) + [sample]]
                 with open(os.path.join(self.experiment_dir, tissue, 'samples', sample), 'w') as f:
                     f.write('\n'.join(sample_vector))
 
     def run_experiment(self):
-        vectors = [[os.path.join(x, 'samples', y)] for x in self.tissue_dirs for y in os.listdir(os.path.join(x, 'samples'))]
+        vectors = [[os.path.join(x, 'samples', y)] for x in self.tissue_dirs
+                   for y in os.listdir(os.path.join(x, 'samples'))]
         blob = zip([self.script_path for _ in xrange(len(vectors))], vectors)
 
         log.info('Starting DESeq2 Runs using {} cores'.format(self.cores))
@@ -66,30 +67,30 @@ class GtexPairwise(AbstractExperiment):
     def reduce(self):
         """Reduce results for each tissue into a single dataframe with p-value counts"""
         log.info('Reducing results for each tissue into a single dataframe of p-value counts')
+        gene_map = pickle.load(open(self.gene_map, 'rb'))
         for tissue_dir in tqdm(self.tissue_dirs):
             results_path = os.path.join(tissue_dir, 'results.tsv')
             ranked = pd.DataFrame()
             pvals = defaultdict(list)
             fc = defaultdict(list)
-            cpm = defaultdict(list)
             results = [os.path.join(tissue_dir, 'results', x) for x in os.listdir(os.path.join(tissue_dir, 'results'))]
-            for result in results:
-                df = add_gene_names(result, self.gene_map)
+            for result in tqdm(results):
+                df = pd.read_csv(result, index_col=0, sep='\t')
                 for gene in df.index:
-                    pvals[gene].append(df.loc[gene]['PValue'])
-                    fc[gene].append(df.loc[gene]['logFC'])
-                    cpm[gene].append(df.loc[gene]['logCPM'])
+                    pvals[gene].append(df.loc[gene]['padj'])
+                    fc[gene].append(df.loc[gene]['log2FoldChange'])
 
             genes = pvals.keys()
+            ranked['num_samples'] = [len(pvals[x]) for x in genes]
             ranked['pval_counts'] = [sum([1 for y in pvals[x] if y < 0.001]) for x in genes]
             ranked['pval'] = [np.median(pvals[x]) for x in genes]
             ranked['pval_std'] = [round(np.std(pvals[x]), 4) for x in genes]
             ranked['fc'] = [round(np.median(fc[x]), 4) for x in genes]
             ranked['fc_std'] = [round(np.std(fc[x]), 4) for x in genes]
-            ranked['cpm'] = [round(np.median(cpm[x]), 4) for x in genes]
-            ranked['cpm_std'] = [round(np.std(cpm[x]), 4) for x in genes]
-            ranked['num_samples'] = [len(pvals[x]) for x in genes]
-            ranked.index = genes
+
+            gene_names = [gene_map[x] if x in gene_map.keys() else x for x in genes]
+            ranked['gene_id'] = genes
+            ranked.index = gene_names
             ranked.sort_values('pval_counts', inplace=True, ascending=False)
 
             ranked.to_csv(results_path)
@@ -99,10 +100,10 @@ class GtexPairwise(AbstractExperiment):
 
     def deseq2_script(self):
         return textwrap.dedent("""
-        library('DESeq2')
+        suppressMessages(library('DESeq2'))
+        suppressMessages(library('data.table'))
 
         args <- commandArgs(trailingOnly = TRUE)
-        # df_path <- args[1]
         df_path <- '{df_path}'
         vector_path <- args[1]
         vector_name <- basename(vector_path)  # Name of vector should be the GTEx sample
@@ -113,7 +114,8 @@ class GtexPairwise(AbstractExperiment):
         vector <- read.table(vector_path)$V1
 
         # Subset dataframe
-        sub <- n[, vector]
+        sub <- n[, colnames(n)%in%vector]
+        setcolorder(sub, as.character(vector))
 
         # Create condition vector
         condition <- c(rep('A', length(sub)-1), 'B')
