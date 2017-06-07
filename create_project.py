496767edbe3f184fc4f0bc72eb9aa3ad60d343c2
John Vivian <jtvivian@gmail.com>
2017-06-06 22:58:33 -0700

    Add attribute to pull file name

--- create_project.py
+++ create_project.py
@@ -70,7 +70,7 @@ def synpase_download(blob):
     syn, root_dir, info = blob
     syn_id, location = info
     download_dir = os.path.join(root_dir, location)
-    if not os.path.exists(os.path.join(download_dir, syn.get(syn_id, downloadFile=False))):
+    if not os.path.exists(os.path.join(download_dir, syn.get(syn_id, downloadFile=False).name)):
         syn.get(syn_id, downloadLocation=download_dir)
 
 
