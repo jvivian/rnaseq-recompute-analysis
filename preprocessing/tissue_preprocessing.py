061085bb55ac1f6906f1c87be28844f3a23b3ff4
John Vivian <jtvivian@gmail.com>
2017-06-07 01:42:37 -0700

    typo

--- preprocessing/tissue_preprocessing.py
+++ preprocessing/tissue_preprocessing.py
@@ -25,7 +25,7 @@ def create_candidate_pairs(df, root_dir):
     # For each candidate tissue (and possible pairing) create a dataframe for downstream clustering
     with open(pair_file, 'r') as f:
         for line in f:
-            line = line.strip().split(',') if ',' in line else [line.stip()]
+            line = line.strip().split(',') if ',' in line else [line.strip()]
 
             # Pull samples that match our tissue
             samples = flatten([list(metadata[metadata.tissue == x].index) for x in line])
